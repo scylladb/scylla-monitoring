10146dee3087ce47a705e0e133cec7d55c2e9613
Fedora Cloud User <fedora@ip-172-31-20-131.us-west-2.compute.internal>
2016-07-13 08:33:44 +0000

    move to offical grafan docker 3.1.0

--- start-all.sh
+++ start-all.sh
@@ -3,8 +3,12 @@
 DB_IP=$1
 
 sudo docker run -d -v $PWD/prometheus/prometheus.yml:/etc/prometheus/prometheus.yml -p 9090:9090 prom/prometheus
-#sudo docker run -d -i -p 3000:3000 -e "GF_AUTH_BASIC_ENABLED=false" -e "GF_AUTH_ANONYMOUS_ENABLED=true" -e "GF_AUTH_ANONYMOUS_ORG_ROLE=Admin" grafana/grafana
-sudo docker run -d -i -p 3000:3000 -e "GF_AUTH_BASIC_ENABLED=false" -e "GF_AUTH_ANONYMOUS_ENABLED=true" -e "GF_AUTH_ANONYMOUS_ORG_ROLE=Admin" graanjonlo/grafana:3.0.0-beta6
+sudo docker run -d -i -p 3000:3000 \
+     -e "GF_AUTH_BASIC_ENABLED=false" \
+     -e "GF_AUTH_ANONYMOUS_ENABLED=true" \
+     -e "GF_AUTH_ANONYMOUS_ORG_ROLE=Admin" \
+     -e "GF_INSTALL_PLUGINS=grafana-piechart-panel" \
+     grafana/grafana:3.1.0
 
 sleep 3
 curl -XPOST -i http://localhost:3000/api/datasources \
