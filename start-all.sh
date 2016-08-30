906b48124d3c337e929c98d0a2d65f06fbfa63eb
Tomasz Grabiec <tgrabiec@scylladb.com>
2016-08-30 14:24:18 +0200

    start-all.sh: Fix permission issue for mounts
    Fixes #43.

--- start-all.sh
+++ start-all.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-sudo docker run -d -v $PWD/prometheus/prometheus.yml:/etc/prometheus/prometheus.yml -p 9090:9090 --name aprom prom/prometheus:v1.0.0
+sudo docker run -d -v $PWD/prometheus/prometheus.yml:/etc/prometheus/prometheus.yml:Z -p 9090:9090 --name aprom prom/prometheus:v1.0.0
+
 sudo docker run -d -i -p 3000:3000 \
      -e "GF_AUTH_BASIC_ENABLED=false" \
      -e "GF_AUTH_ANONYMOUS_ENABLED=true" \
