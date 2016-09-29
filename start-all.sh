ab660fa5b0d739c63a43cd0f9ffc08269fe4ba3c
Tzach Livyatan <tzach@scylladb.com>
2016-09-29 13:19:58 +0300

    add option to load prometheus data from a dir

--- start-all.sh
+++ start-all.sh
@@ -1,6 +1,32 @@
 #!/usr/bin/env bash
 
-sudo docker run -d -v $PWD/prometheus/prometheus.yml:/etc/prometheus/prometheus.yml:Z -p 9090:9090 --name aprom prom/prometheus:v1.0.0
+usage="$(basename "$0") [-h] [-d Prometheus data-dir] -- starts Grafana and Prometheus Docker instances"
+
+while getopts ':hd:' option; do
+  case "$option" in
+    h) echo "$usage"
+       exit
+       ;;
+    d) DATA_DIR=$OPTARG
+       ;;
+    :) printf "missing argument for -%s\n" "$OPTARG" >&2
+       echo "$usage" >&2
+       exit 1
+       ;;
+   \?) printf "illegal option: -%s\n" "$OPTARG" >&2
+       echo "$usage" >&2
+       exit 1
+       ;;
+  esac
+done
+
+if [ -z $DATA_DIR ]
+then
+    sudo docker run -d -v $PWD/prometheus/prometheus.yml:/etc/prometheus/prometheus.yml:Z -p 9090:9090 --name aprom prom/prometheus:v1.0.0
+else
+    echo "Loading prometheus data from $DATA_DIR"
+    sudo docker run -d -v $DATA_DIR:/prometheus -v $PWD/prometheus/prometheus.yml:/etc/prometheus/prometheus.yml:Z -p 9090:9090 --name aprom prom/prometheus:v1.0.0
+fi
 
 sudo docker run -d -i -p 3000:3000 \
      -e "GF_AUTH_BASIC_ENABLED=false" \
