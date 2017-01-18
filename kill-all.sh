29631108d5c66714f8cec6f0341643427de7e9ef
Glauber Costa <glommer@scylladb.com>
2017-01-17 21:31:46 -0500

    add options for prometheus and grafana ports
    We sometimes have the need to monitor many deployments at once.
    Instead of having to kill the current containers, it would be useful
    to let them live alongside each other in different ports.
    
    To do that, we need grafana and prometheus to be able to listen to
    different ports and have different names.
    
    Signed-off-by: Glauber Costa <glommer@scylladb.com>

--- kill-all.sh
+++ kill-all.sh
@@ -1,4 +1,38 @@
 #!/usr/bin/env bash
 
-sudo docker kill agraf aprom
-sudo docker rm agraf aprom
+usage="$(basename "$0") [-h] [-g grafana port ] [ -p prometheus port ] -- kills existing Grafana and Prometheus Docker instances at given ports"
+
+while getopts ':hg:p:' option; do
+  case "$option" in
+    h) echo "$usage"
+       exit
+       ;;
+    g) GRAFANA_PORT=$OPTARG
+       ;;
+    p) PROMETHEUS_PORT=$OPTARG
+       ;;
+    :) printf "missing argument for -%s\n" "$OPTARG" >&2
+       echo "$usage" >&2
+       exit 1
+       ;;
+   \?) printf "illegal option: -%s\n" "$OPTARG" >&2
+       echo "$usage" >&2
+       exit 1
+       ;;
+  esac
+done
+
+if [ -z $GRAFANA_PORT ]; then
+    GRAFANA_NAME=agraf
+else
+    GRAFANA_NAME=agraf-$GRAFANA_PORT
+fi
+if [ -z $PROMETHEUS_PORT ]; then
+    PROMETHEUS_NAME=aprom
+else
+    PROMETHEUS_NAME=aprom-$PROMETHEUS_PORT
+fi
+
+
+sudo docker kill $GRAFANA_NAME $PROMETHEUS_NAME
+sudo docker rm $GRAFANA_NAME $PROMETHEUS_NAME
