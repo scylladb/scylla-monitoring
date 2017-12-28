98ce1829530e17fbc251477119f62a2eb22336bc
Matt Chapman <Matt@NinjitsuWeb.com>
2017-12-27 22:11:22 -0800

    Make Datasource name configurable.

--- load-grafana.sh
+++ load-grafana.sh
@@ -4,8 +4,11 @@
 VERSIONS=$DEFAULT_VERSION
 GRAFANA_PORT=3000
 DB_ADDRESS="127.0.0.1:9090"
+DATASOURCE_NAME='prometheus'
 
-usage="$(basename "$0") [-h] [-v comma separated versions ] [-g grafana port ] [-p ip:port address of prometheus ] [-a admin password] [-j additional dashboard to load to Grafana, multiple params are supported] -- loads the prometheus datasource and the Scylla dashboards into an existing grafana installation"
+usage="$(basename "$0") [-h] [-v comma separated versions ] [-g grafana port ] [-p ip:port address of prometheus ] [-a admin password] [-j additional dashboard to load to Grafana, multiple params are supported] [-d name to use for added datasource]
+
+loads the prometheus datasource and the Scylla dashboards into an existing grafana installation"
 
 while getopts ':hg:p:v:a:j:' option; do
   case "$option" in
@@ -22,24 +25,28 @@ while getopts ':hg:p:v:a:j:' option; do
        ;;
     a) GRAFANA_ADMIN_PASSWORD=$OPTARG
        ;;
+    d) DATASOURCE_NAME=$OPTARG
+       ;;
   esac
 done
 
 curl -XPOST -i http://admin:$GRAFANA_ADMIN_PASSWORD@localhost:$GRAFANA_PORT/api/datasources \
-     --data-binary '{"name":"prometheus", "type":"prometheus", "url":"'"http://$DB_ADDRESS"'", "access":"proxy", "basicAuth":false}' \
+     --data-binary '{"name":"'$DATASOURCE_NAME'", "type":"prometheus", "url":"'"http://$DB_ADDRESS"'", "access":"proxy", "basicAuth":false}' \
      -H "Content-Type: application/json"
 
 mkdir -p grafana/build
+sed "s/DATASOURCE_NAME/$DATASOURCE_NAME/" ./grafana/types.json > ./grafana/build/types.json
 IFS=',' ;for v in $VERSIONS; do
 for f in scylla-dash scylla-dash-per-server scylla-dash-io-per-server; do
 	if [ -e grafana/$f.$v.template.json ]
 	then
-		./make_dashboards.py -t grafana/types.json -d grafana/$f.$v.template.json
-		curl -XPOST -i http://admin:$GRAFANA_ADMIN_PASSWORD@localhost:$GRAFANA_PORT/api/dashboards/db --data-binary @./grafana/build/$f.$v.json -H "Content-Type: application/json"
-  else
-    curl -XPOST -i http://admin:$GRAFANA_ADMIN_PASSWORD@localhost:$GRAFANA_PORT/api/dashboards/db --data-binary @./grafana/$f.$v.json -H "Content-Type: application/json"  
+		./make_dashboards.py -t grafana/build/types.json -d grafana/$f.$v.template.json
+        sed -i "s/DATASOURCE_NAME/$DATASOURCE_NAME/" ./grafana/build/$f.$v.json
+    else
+        sed "s/DATASOURCE_NAME/$DATASOURCE_NAME/" ./grafana/$f.$v.json > ./grafana/build/$f.$v.json
 	fi
-	
+	curl -XPOST -i http://admin:$GRAFANA_ADMIN_PASSWORD@localhost:$GRAFANA_PORT/api/dashboards/db --data-binary @./grafana/build/$f.$v.json -H "Content-Type: application/json"
+
 done
 done
 
