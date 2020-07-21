31ec10e6d119562596663bd1808d817a4ff66466
dgarcia360 <dgarcia360@outlook.com>
2020-07-21 19:14:50 +0200

    Added gh actions, Replaced _utils file, match base project structure

--- docs/_utils/deploy.sh
+++ docs/_utils/deploy.sh
@@ -1,16 +1,15 @@
-#!/bin/sh -e
+#!/bin/bash
 
-ORIGIN="https://github.com/scylladb/scylla-monitoring.git"
-BUILD="_build/dirhtml"
-
-# Make a git repo for the built pages
-rm -rf _build/dirhtml/.git
-(cd $BUILD && git init && git checkout -b gh-pages)
-
-# Set up Git and GitHub Pages for the built pages repo
-cp .git/config _build/dirhtml/.git
-cp CNAME _build/dirhtml/
-
-# Commit and push
-(cd $BUILD && git add . && git commit -a -m "scripted deploy, will be overwritten")
-(cd $BUILD && git push -f $ORIGIN gh-pages)
+# Clone repo
+git clone "https://x-access-token:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git" --branch gh-pages --single-branch gh-pages
+cp -r docs/_build/dirhtml/* gh-pages
+# Redirect index to master
+cp docs/_utils/redirect.html gh-pages/index.html
+# Deploy
+cd gh-pages
+touch .nojekyll
+git config --local user.email "action@scylladb.com"
+git config --local user.name "GitHub Action"
+git add .
+git commit -m "Publish docs" || true
+git push origin gh-pages --force
