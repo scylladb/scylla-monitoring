78b30b9aabb8a81a792d7cee3d4ce33ea9c767b6
David Garcia <dgarcia360@outlook.com>
2020-08-29 19:29:04 +0200

    Redirect to latest version (#1021)
    * Redirect to latest version
    
    * space

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -539,7 +539,7 @@ sitemap_url_scheme = "{link}"
 # smv_tag_whitelist = r'^.*$'
 smv_tag_whitelist = r'\b(scylla-monitoring-3.4)\b'
 # Whitelist pattern for branches (set to None to ignore all branches)
-smv_branch_whitelist = r"^master$"
+smv_branch_whitelist = "None"
 # Whitelist pattern for remotes (set to None to use local branches only)
 smv_remote_whitelist = r"^origin$"
 # Pattern for released versions
