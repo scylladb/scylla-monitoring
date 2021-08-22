b9b76c1112aa560ad57cbe816a04b0debe8ab02e
Amnon Heiman <amnon@scylladb.com>
2021-08-22 17:40:54 +0300

    conf.py set the current version in the doc to 3.8.2

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -94,7 +94,7 @@ def setup(sphinx):
 # Adds version variables for monitoring and manager versions when used in inline text
 
 rst_prolog = """
-.. |version| replace:: 3.8.0
+.. |version| replace:: 3.8.2
 .. |mon_root| replace::  `Scylla Monitoring Stack </>`__
 """
 
