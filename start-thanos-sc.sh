1fe53d7aba216628765be8340fced351680cac38
Amnon Heiman <amnon@scylladb.com>
2021-09-02 12:25:34 +0300

    start-thanos-sc: support bind and port mapping

--- start-thanos-sc.sh
+++ start-thanos-sc.sh
@@ -11,14 +11,15 @@ else
     USER_PERMISSIONS="-u $UID:$GROUPID"
 fi
 function usage {
-  __usage="Usage: $(basename $0) [-h] [-d /path/to/dir] [-a ip:port]
+  __usage="Usage: $(basename $0) [-h] [-d /path/to/dir] [-a ip:port] [-A ip]
 
 Options:
   -h print this help and exit
   -d path/to/Prometheus/data/dir - Prometheus  external data directory, must be used
   -a prometheus address          - Prometheus address:port
+  -A address                     - bind to a specific ip address
 
-The script starts Thanos sidecart, it will read from Prometheus directory, so that directory must be external 
+The script starts Thanos sidecart, it will read from Prometheus directory, so that directory must be external
 "
   echo "$__usage"
 }
@@ -29,7 +30,7 @@ if [ ! -z "$is_podman" ]; then
     group_args+=(--userns=keep-id)
 fi
 
-while getopts ':hl:p:a:d:n:' option; do
+while getopts ':hl:p:a:d:A:n:' option; do
   case "$option" in
     l) DOCKER_PARAM="$DOCKER_PARAM --net=host"
        ;;
@@ -42,6 +43,10 @@ while getopts ':hl:p:a:d:n:' option; do
        ;;
     a) PROM_ADRESS=$OPTARG
 	   ;;
+	p) THANOS_SC_PORT=$OPTARG
+       ;;
+    A) BIND_ADDRESS="$OPTARG:"
+       ;;
     :) printf "missing argument for -%s\n" "$OPTARG" >&2
        echo "$usage" >&2
        exit 1
@@ -59,10 +64,28 @@ then
 else
     DATA_DIR="-v "$(readlink -m $DATA_DIR)":/data/prom$NAME:z"
 fi
+if [[ $DOCKER_PARAM = *"--net=host"* ]]; then
+    if [ ! -z "$ALERTMANAGER_PORT" ] || [ ! -z "$GRAFANA_PORT" ] || [ ! -z $PROMETHEUS_PORT ]; then
+        echo "Port mapping is not supported with host network, remove the -l flag from the command line"
+        exit 1
+    fi
+    HOST_NETWORK=1
+fi
+if [ -z "$BIND_ADDRESS" ]; then
+  BIND_ADDRESS=""
+fi
+if [ -z "$THANOS_SC_PORT"]; then
+    THANOS_SC_PORT="10911"
+fi
+
+if [ -z $HOST_NETWORK ]; then
+    PORT_MAPPING="-p $BIND_ADDRESS$THANOS_SC_PORT:10911"
+fi
+
 echo "Starting Thanos sidecar"
 docker run -d $DOCKER_PARAM $USER_PERMISSIONS \
      $DATA_DIR \
-     -i --name sidecar$NAME thanosio/thanos:$THANOS_VERSION \
+     -i $PORT_MAPPING --name sidecar$NAME thanosio/thanos:$THANOS_VERSION \
         "sidecar" \
        "--debug.name=sidecar-$NAME" \
        "--log.level=debug" \
@@ -72,4 +95,4 @@ docker run -d $DOCKER_PARAM $USER_PERMISSIONS \
        "--http-grace-period=1s" \
        "--prometheus.url=http://$PROM_ADRESS" \
        "--tsdb.path=/data/prom$NAME" \
-       
+
