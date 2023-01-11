7e626053709df2b26da5afa73a96b284731b1f39
Amnon Heiman <amnon@scylladb.com>
2023-01-11 13:05:18 +0200

    kill-all.sh adding victoria-metrics

--- kill-all.sh
+++ kill-all.sh
@@ -52,6 +52,7 @@ sleep 2
 ./kill-container.sh $GRAFANA_PORT -b agraf
 ./kill-container.sh $ALERTMANAGER_PORT -b aalert
 ./kill-container.sh -b agrafrender
+./kill-container.sh -b vmalert
 ./kill-container.sh -b loki
 ./kill-container.sh -b promtail
 ./kill-container.sh -b sidecar1
