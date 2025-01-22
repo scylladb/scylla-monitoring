05a14a67835a00e3a8b58d06fd444b4cb31d24ba
Amnon Heiman <amnon@scylladb.com>
2025-01-22 10:21:11 +0200

    start-thanos-sc.sh: Allow override the default ports

--- start-thanos-sc.sh
+++ start-thanos-sc.sh
@@ -22,6 +22,8 @@ Options:
   -d path/to/Prometheus/data/dir - Prometheus  external data directory, must be used
   -a prometheus address          - Prometheus address:port
   -A address                     - bind to a specific ip address
+  -p port                        - Override the default port (10911)
+  -H port                        - Override the default http-port (10912)
 
 The script starts Thanos sidecart, it will read from Prometheus directory, so that directory must be external
 "
@@ -140,21 +142,21 @@ else
 	DATA_DIR="-v "$(readlink -m $DATA_DIR)":/data/prom$NAME:z"
 fi
 if [[ $DOCKER_PARAM = *"--net=host"* ]]; then
-	if [ ! -z "$ALERTMANAGER_PORT" ] || [ ! -z "$GRAFANA_PORT" ] || [ ! -z $PROMETHEUS_PORT ]; then
-		echo "Port mapping is not supported with host network, remove the -l flag from the command line"
-		exit 1
-	fi
 	HOST_NETWORK=1
 fi
 if [ -z "$BIND_ADDRESS" ]; then
 	BIND_ADDRESS=""
 fi
-if [ -z "$THANOS_SC_PORT"]; then
+if [ -z "$THANOS_SC_PORT" ]; then
 	THANOS_SC_PORT="10911"
 fi
-
-if [ -z $HOST_NETWORK ]; then
-	PORT_MAPPING="-p $BIND_ADDRESS$THANOS_SC_PORT:10911"
+if [ -z "$THANOS_SC_PORT"  ]; then
+    THANOS_SC_HTTP_PORT="10912"
+fi
+if [ -z "$HOST_NETWORK" ]; then
+	PORT_MAPPING="-p $BIND_ADDRESS$THANOS_SC_PORT:10911 -p $BIND_ADDRESS$THANOS_SC_HTTP_PORT:10912"
+	THANOS_SC_PORT="10911"
+	THANOS_SC_HTTP_PORT="10912"
 fi
 
 echo "Starting Thanos sidecar"
@@ -165,9 +167,9 @@ docker run ${DOCKER_LIMITS["sidecar"]} -d $DOCKER_PARAM $USER_PERMISSIONS \
 	"sidecar" \
 	"--debug.name=$NAME" \
 	${DOCKER_PARAMS["sidecar"]} \
-	"--grpc-address=0.0.0.0:10911" \
+	"--grpc-address=0.0.0.0:$THANOS_SC_PORT" \
 	"--grpc-grace-period=1s" \
-	"--http-address=0.0.0.0:10912" \
+	"--http-address=0.0.0.0:$THANOS_SC_HTTP_PORT" \
 	"--http-grace-period=1s" \
 	"--prometheus.url=http://$PROM_ADRESS" \
 	"--tsdb.path=/data/prom$NAME"
